1e4a498782e2f7e11f3485f4ae0366c833644929
Sterling Hanenkamp <sterling@hanenkamp.com>
2016-12-21 00:24:52 -0600

    Cleaned up my vim directory

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ function __mkdir { if [[ ! -d $1 ]]; then mkdir -p $1; fi }
 function link-file { __mkdir "${2:h}"; rm -rf "$2"; ln -s "$PWD/$1" "$2" }
 function copy-file { __mkdir "${2:h}"; rm -rf "$2"; cp "$PWD/$1" "$2" }
 
-for x in vimrc vim do
-    link-file $x ~/.$x
-done
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:=$HOME/.config}
+
+link-file vim ~/.vim
+link-file vim/init.vim ~/.vimrc
+link-file vim $XDG_CONFIG_HOME/nvim
+
