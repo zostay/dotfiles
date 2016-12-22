b32bd3c69946157e896f14417abd5f7290ef4cb1
Sterling Hanenkamp <sterling@hanenkamp.com>
2016-12-22 00:47:42 -0600

    Adding gitconfig and templating of dotfiles

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/zsh
 
-# Thanks, Frew!
+# Thanks, Frew, for the inspiration!
 
-echo "running rc installer"
+echo "Installing dotfiles."
 
 function __mkdir { if [[ ! -d $1 ]]; then mkdir -p $1; fi }
 function link-file { __mkdir "${2:h}"; rm -rf "$2"; ln -s "$PWD/$1" "$2" }
 function copy-file { __mkdir "${2:h}"; rm -rf "$2"; cp "$PWD/$1" "$2" }
+function tmpl-file { __mkdir ".build"; template-dotfile $DOTFILE_ENV "$1" ".build/$1" }
+function tmpl-link-file { tmpl-file "$1"; link-file "$1" "$2" }
 
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:=$HOME/.config}
+if [[ -n $* ]]; then
+    DOTFILE_ENV=$1;
+    echo $DOTFILE_ENV >! ~/.dotfile-environment
+    shift;
+else
+    DOTFILE_ENV=$(cat ~/.dotfile-environment)
+fi
 
 link-file bin ~/bin
 
@@ -24,3 +33,7 @@ link-file vim $XDG_CONFIG_HOME/nvim
 link-file zsh ~/.zsh
 link-file zshrc ~/.zshrc
 link-file zshenv ~/.zshenv
+
+tmpl-link-file gitconfig ~/.gitconfig
+
+
