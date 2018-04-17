2a28a6b47c565ac6dffda89c2cb1a5ceb4c52a46
Sterling Hanenkamp <sterling@hanenkamp.com>
2018-04-17 09:52:03 -0500

    Cleanup

--- bin/256colors.pl
+++ bin/256colors.pl
@@ -1,29 +1,29 @@
-#!/usr/bin/perl
-# Author: Todd Larason <jtl@molehill.org>
-# $XFree86: xc/programs/xterm/vttests/256colors2.pl,v 1.1 1999/07/11 08:49:54 dawes Exp $
+#!/usr/bin/env perl
+# Original Author: Todd Larason <jtl@molehill.org>
+# Heavily modified by Andrew Sterling Hanenkamp.
 
-# use the resources for colors 0-15 - usually more-or-less a
-# reproduction of the standard ANSI colors, but possibly more
-# pleasing shades
+use v5.20;
+use warnings;
 
 # colors 16-231 are a 6x6x6 color cube
-for ($red = 0; $red < 6; $red++) {
-    for ($green = 0; $green < 6; $green++) {
-        for ($blue = 0; $blue < 6; $blue++) {
-            printf("\x1b]4;%d;rgb:%2.2x/%2.2x/%2.2x\x1b\\",
-            16 + ($red * 36) + ($green * 6) + $blue,
-            int ($red * 42.5),
-            int ($green * 42.5),
-            int ($blue * 42.5));
+for my $red (0 .. 5) {
+    for my $green (0 .. 5) {
+        for my $blue (0 .. 5) {
+            my $i = 16 + ($red * 36) + ($green * 6) + $blue;
+            my $r = int ($red * 42.5);
+            my $g = int ($green * 42.5);
+            my $b = int ($blue * 42.5);
+            say "$i <- ($r, $g, $b)";
+            printf("\e]4;%d;rgb:%2.2x/%2.2x/%2.2x\e\\", $i, $r, $g, $b);
         }
     }
 }
 
 # colors 232-255 are a grayscale ramp, intentionally leaving out
 # black and white
-for ($gray = 0; $gray < 24; $gray++) {
-    $level = ($gray * 10) + 8;
-    printf("\x1b]4;%d;rgb:%2.2x/%2.2x/%2.2x\x1b\\",
+for my $gray (0 .. 23) {
+    my $level = ($gray * 10) + 8;
+    printf("\e]4;%d;rgb:%2.2x/%2.2x/%2.2x\e\\",
 	   232 + $gray, $level, $level, $level);
 }
 
@@ -32,24 +32,24 @@ for ($gray = 0; $gray < 24; $gray++) {
 
 # first the system ones:
 print "System colors:\n";
-for ($color = 0; $color < 8; $color++) {
-    print "\x1b[48;5;${color}m  ";
+for my $color (0 .. 7) {
+    print "\e[48;5;${color}m  ";
 }
-print "\x1b[0m\n";
-for ($color = 8; $color < 16; $color++) {
-    print "\x1b[48;5;${color}m  ";
+print "\e[0m\n";
+for my $color (8 .. 15) {
+    print "\e[48;5;${color}m  ";
 }
-print "\x1b[0m\n\n";
+print "\e[0m\n\n";
 
 # now the color cube
 print "Color cube, 6x6x6:\n";
-for ($green = 0; $green < 6; $green++) {
-    for ($red = 0; $red < 6; $red++) {
-        for ($blue = 0; $blue < 6; $blue++) {
-            $color = 16 + ($red * 36) + ($green * 6) + $blue;
-            print "\x1b[48;5;${color}m  ";
+for my $green (0 .. 5) {
+    for my $red (0 .. 5) {
+        for my $blue (0 .. 5) {
+            my $color = 16 + ($red * 36) + ($green * 6) + $blue;
+            print "\e[48;5;${color}m  ";
         }
-        print "\x1b[0m ";
+        print "\e[0m ";
     }
     print "\n";
 }
@@ -57,7 +57,7 @@ for ($green = 0; $green < 6; $green++) {
 
 # now the grayscale ramp
 print "Grayscale ramp:\n";
-for ($color = 232; $color < 256; $color++) {
-    print "\x1b[48;5;${color}m  ";
+for my $color (232 .. 255) {
+    print "\e[48;5;${color}m  ";
 }
-print "\x1b[0m\n";
+print "\e[0m\n";
