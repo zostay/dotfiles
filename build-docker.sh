d12793fe16a708204d48a3429d85c098f9422d52
Sterling Hanenkamp <sterling@hanenkamp.com>
2020-03-19 00:10:16 -0500

    Adding a dotfiles-raku docker

--- build-docker.sh
+++ build-docker.sh
@@ -4,3 +4,6 @@ DOCKER_DIR=docker
 
 docker build $DOCKER_DIR/perl -t zostay/dotfiles-perl
 docker push zostay/dotfiles-perl
+
+docker build $DOCKER_DIR/raku -t zostay/dotfiles-raku
+docker push zostay/dotfiles-raku
